9b7af8c16f6de5b4bd81a0215a41a896f5d318d6
Franco030 <edwinfranco3104@hotmail.com>
2025-12-27 15:47:44 -0600

    The heart of the VM is implemented

---
+++ vm.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+#include <stdio.h>
+
+#include "debug.h"
+
+VM vm;
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {
+
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+/*
+ * This is the single most important function in all of cfer, by far.
+ * When te interpreter executes a user's program, it will spend something like 90% of its time inside run().
+ * It is the beating heart of the VM.
+ *
+ * We have an outer loop that goes and goes. Each turn through that loop, we rean and execute a single bytecode instruction.
+ * To process an instruction, we first figure out what kind of instruction we're dealing with.
+ * The READ_BYTE macro reads the byte currently pointed at by ip and then advances the instruction pointer.
+ * The first byte on any instruction is the opcode. Given a numeric opcode, we need to get to the right C code that implements
+ * that instruction's semantics. This process is called decoding or dispatching the instruction.
+ * Note that ip advances as soon as we read the opcode, before we've actually started executing the instruction.
+ * So, again, ip points to the next byte of code to be used. How (*vm.ip++) works:
+ * Even though it looks simple, C operator precedence rules are strict here. The expression is evaluated in this specific order:
+ *  1. vm.ip++ (Post-increment): The ++ operator has higher precedence than *. Becuase it is a post-increment, it effectively says:
+ *  "Remember the current value of ip for the rest of this expression, but increase the actual variable ip by 1 immediately after."
+ *  2. * (Dereference): The code dereferences the original address (the one remembered before the increment happened)
+ * It's the same as writing:
+ * uint8_t instruction = *vm.ip;
+ * vm.ip++;
+ * But macros avoid the overhead of a function call.
+ *
+ * We do that process for every single instruction, every single time one is executed,
+ * so this is the most performance critical part of the entire virtual machine.
+ * There are clever techniques to do bytecode dispatch efficientely, but the fastest solutions require either non-standard extensions to C,
+ * or handwritten assembly code. We'll keep it simple for now. Just like our disassembler, we have a single giant switch statement with a case for each opcode.
+ * The body of each case implements that opcode's behavior.
+ *
+ */
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        disassembleInstruction(vm.chunk, (int)(vm.ip-vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+        }
+    }
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+/*
+ * First we store the chunk being executed in the VM.
+ * Then we call run(), and internal helper function that actually runs the bytecode instructions.
+ * Between those two parts there's 'ip'.
+ *
+ * As the VM works its way through the bytecode, it keeps track of where it is,
+ * the location of the instruction currently being executed.
+ * We don't use a local variable inside run() for this because eventually other functions will need to access it.
+ */
+
+InterpretResult interpret(Chunk *chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
---
+++ vm.h
@@ -0,0 +1,103 @@
+#ifndef CFER_VM_H
+#define CFER_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+/*
+ * The virtual machine is one part of our interpreter's internal architecture.
+ * We hand it a chunk of code, literally a Chunk, and it runs it.
+ *
+ * As the VM works its way through the bytecode, it keeps track of where it is,
+ * the location of the instruction currently being executed.
+ * We don't use a local variable inside run() for this because eventually other functions will need to access it.
+ *
+ * Its type is a byte pointer. We use an actual real C pointer right into the middle of the bytecode array
+ * instead of something like an integeer index because it's faster to dereference a pointer than look up an element
+ * in an array by index.
+ *
+ * The name "IP" is traditional, it's an instruction pointer.
+ * We initialize ip by pointing it at the first byte of code in the chunk. We haven't executed that instruction yes,
+ * so ip points to the instruction about to be executed. This will be true durint the entire time the VM is running:
+ * the IP always points to the next instruction, not the one currently being handled.
+ *
+ * Our compiled bytecode needs a way to shuttle values around between the different instructions that need them.
+ * For example:
+ * print 3 - 2;
+ * We obviously need instructions for the constants 3 and 2, the print statement, and the substraction.
+ * But how does the substracion know that 3 is the minuend and 2 is the subtrahend? How does the print instruction know to print the result of that?
+ * The operands to an arithmetic operator obviouly need to be evaluated before we can perform the operation itself.
+ *
+ * Since the temporary values we need to track naturally have stack-like behavior,
+ * our VM will use a stack to manage them. When it needs to consume one or more values,
+ * it gets them by popping them off the stack.
+ *
+ * We implement the stack semantics ourselves on top of a raw C array.
+ * The bottom of the stack, the first value pushed and the last to be popped, is at element zero in the array,
+ * and later pushed values follow it. If we push the letters of "hello" on to the stack, in order,
+ * the resulting C array looks like this:
+ *
+ *         0   1   2   3   4   5
+ * bottom [h] [e] [l] [l] [o] [ ] top
+ *
+ * Since the stack grows and shrinks as values are pushed and popped, we need to track where the top of the stack in in the array.
+ * As with ip, we use a direct pointer instead of an integer index since it's faster to dereference the pointer than calculate the offset
+ * from the index each time we need it.
+ *
+ * The pointer points at the array element just past the element containing the top value on the stack.
+ * That seems a little odd, but almost every implementation does this. It means we can indicate that the stack is empty
+ * by pointing at element zero in the array.
+ *  0        1   2   3   4   5
+ * [ ]      [ ] [ ] [ ] [ ] [ ]
+ * stackTop
+ *
+ * If we pointed to the top element, then for an empty stack we'd need to point at element -1. That's undefined in C. As we push values onto the stack...
+ *  0   1        2   3   4   5
+ * [h] [ ]      [ ] [ ] [ ] [ ]
+ *     stackTop
+ *
+ * ...stackTop always points just past the last item
+ * The maximum number of values we can store on the stack (for now, at least) is 256.
+ *
+ * Giving out VM a fixed stack size means it's possible for some sequence of instructions to push too many values and run out of stack space.
+ * We could grow the stack dynamically as needed, but for now, we'll keep it simple.
+ */
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value *stackTop;
+} VM;
+
+/*
+ * The VM runs the chunk and then responds with a value from this enum.
+ * When the compiler reports static errors and the VM detect runtime errors,
+ * the interpreter will use this to know how to set the exit code of the process.
+ */
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+/*
+ * Like we do with most of the data structures we create,
+ * we also define functions to create and tear down a VM.
+ */
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk *chunk);
+
+/*
+ * The stack protocol supports two operations
+ */
+
+void push(Value value);
+Value pop();
+
+#endif //CFER_VM_H
