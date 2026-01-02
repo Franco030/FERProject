7811ec2af0f3dcd4bab14c0fbf659998930c47c4
Franco030 <edwinfranco3104@hotmail.com>
2026-01-02 13:32:02 -0600

    Ifs implemented.

--- compiler.c
+++ compiler.c
@@ -200,6 +200,13 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
     emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction) {
+    emitByte(instruction);
+    emitByte(0xff);
+    emitByte(0xff);
+    return currentChunk()->count - 2; // Remember that count points to the next empty space after the second 0xff
+}
+
 static void emitReturn() {
     emitByte(OP_RETURN);
 }
@@ -216,6 +223,18 @@ static uint8_t makeConstant(Value value) {
 
 static void emitConstant(Value value) {
     emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void patchJump(int offset) {
+    // -2 to adjust for the bytecode for the jump offset itself
+    int jump = currentChunk()->count - offset - 2;
+
+    if (jump > UINT16_MAX) {
+        error("Too much code to jump over.");
+    }
+
+    currentChunk()->code[offset] = (jump >> 8) & 0xff;
+    currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
 static void initCompiler(Compiler *compiler) {
@@ -324,6 +343,14 @@ static void defineVariable(uint8_t global) {
     }
 
     emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void and_(bool canAssign) {
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
 }
 
 /*
@@ -449,6 +476,17 @@ static void number(bool canAssign) {
     emitConstant(NUMBER_VAL(value));
 }
 
+static void or_(bool canAssign) {
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
+}
+
 /*
  * This takes the string's characters directly from the lexeme.
  * The + 1 and - 2 parts trim the leading and trailing quotation marks. It then creates a string object,
@@ -555,7 +593,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]      = {variable, NULL,          PREC_NONE},
     [TOKEN_STRING]          = {string,   NULL,          PREC_NONE},
     [TOKEN_NUMBER]          = {number,   NULL,          PREC_NONE},
-    [TOKEN_AND]             = {NULL,     NULL,          PREC_NONE},
+    [TOKEN_AND]             = {NULL,     and_,          PREC_NONE},
     [TOKEN_CLASS]           = {NULL,     NULL,          PREC_NONE},
     [TOKEN_ELSE]            = {NULL,     NULL,          PREC_NONE},
     [TOKEN_FALSE]           = {literal,  NULL,          PREC_NONE},
@@ -563,7 +601,7 @@ ParseRule rules[] = {
     [TOKEN_FUN]             = {NULL,     NULL,          PREC_NONE},
     [TOKEN_IF]              = {NULL,     NULL,          PREC_NONE},
     [TOKEN_NIL]             = {literal,  NULL,          PREC_NONE},
-    [TOKEN_OR]              = {NULL,     NULL,          PREC_NONE},
+    [TOKEN_OR]              = {NULL,     or_,           PREC_NONE},
     [TOKEN_PRINT]           = {NULL,     NULL,          PREC_NONE},
     [TOKEN_RETURN]          = {NULL,     NULL,          PREC_NONE},
     [TOKEN_SUPER]           = {NULL,     NULL,          PREC_NONE},
@@ -672,6 +710,22 @@ static void expressionStatement() {
     emitByte(OP_POP);
 }
 
+static void ifStatement() {
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    int thenJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    int elseJump = emitJump(OP_JUMP);
+
+    patchJump(thenJump);
+    emitByte(OP_POP);
+
+    if (match(TOKEN_ELSE)) statement();
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -717,6 +771,8 @@ static void statement() {
         beginScope();
         block();
         endScope();
+    } else if (match(TOKEN_IF)) {
+        ifStatement();
     } else {
         expressionStatement();
     }
