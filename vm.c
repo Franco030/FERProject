8cf62a386b1a6793eb4553b366b06ffda6165ba0
Franco030 <edwinfranco3104@hotmail.com>
2026-01-06 17:09:46 -0600

    Added string convertion for numbers and booleans.

--- vm.c
+++ vm.c
@@ -20,6 +20,31 @@ static Value clockNative(int argCount, Value *args) {
     return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
 }
 
+static Value strNative(int argCount, Value *args) {
+    if (argCount != 1) {
+        return NIL_VAL;
+    }
+
+    Value value = args[0];
+
+    if (IS_STRING(value)) {
+        return value;
+    }
+
+    char buffer[1024];
+    int length = 0;
+
+    if (IS_BOOL(value)) {
+        length = sprintf(buffer, "%s", AS_BOOL(value) ? "true" : "false");
+    } else if (IS_NIL(value)) {
+        length = sprintf(buffer, "nil");
+    } else if (IS_NUMBER(value)) {
+        length = sprintf(buffer, "%g", AS_NUMBER(value));
+    }
+
+    return OBJ_VAL(copyString(buffer, length));
+}
+
 static void resetStack() {
     vm.stackTop = vm.stack;
     vm.frameCount = 0;
@@ -88,6 +113,7 @@ void initVM() {
     vm.initString = copyString("init", 4);
 
     defineNative("clock", clockNative);
+    defineNative("str", strNative);
 }
 
 void freeVM() {
